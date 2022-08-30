c1d8a4719528022280d4a39ce18dee240cbd6853
mario arriola <riblarsv@gmail.com>
2022-08-30 07:20:33 +0600

    added the class components and the functional components
    this now is working

---
+++ src/components/ChangeText/ChangeText.jsx
@@ -0,0 +1,28 @@
+import {Component} from "react";
+import { btnStyles } from "./styles";
+
+class ChangeText extends Component{
+    constructor(){
+        super()
+        this.state={text:"hello world"}
+        // this.changeTheText = this.changeTheText.bind(this)
+    }
+    changeTheText= () => {
+        this.setState({text:"hello My friends"})
+    }
+    render(){
+        const {text} = this.state
+        return(
+            <div className="container">
+                <h1>{text}</h1>
+                <button 
+                onClick={this.changeTheText} style={btnStyles}
+                >
+                    Change Text
+                </button>
+            </div>
+        )
+    }
+}
+
+export default ChangeText
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header({name,age}){
+    return(
+        <>
+            <h1>header {name} {age}</h1>
+        </>
+    )
+}
+export default Header
